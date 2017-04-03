17d4b48b6bad264d9ebb4dad7a90c018c75932dd
Dave Bush <github@david-bush.co.uk>
2017-04-03 16:07:19 +1200

    Began refactor of unit tests.

--- tests/test_email_backend.py
+++ tests/test_email_backend.py
@@ -1,6 +1,7 @@
 import pytest
 from django.core.mail import EmailMessage
 
+from smtp2go.exceptions import Smtp2goParameterException
 from smtp2go_django.email_backend import (
     Smtp2goAPIContentException,
     Smtp2goEmailBackend,
@@ -26,66 +27,66 @@ def test_messages():
 TEST_API_KEY = 'testapikey'
 
 
-def test_missing_to_field(monkeypatch, test_messages):
-    monkeypatch.setenv('SMTP2GO_API_KEY', TEST_API_KEY)
-    email_backend = Smtp2goEmailBackend()
-    for message in test_messages:
-        setattr(message, 'to', None)
-    # assert not any([getattr(message, 'to', None) message in test_messages])
-    with pytest.raises(Smtp2goAPIContentException):
-        email_backend.send_messages(test_messages)
+class TestEmailBackend:
+    def test_missing_to_field(self, monkeypatch, test_messages):
+        monkeypatch.setenv('SMTP2GO_API_KEY', TEST_API_KEY)
+        email_backend = Smtp2goEmailBackend()
+        for message in test_messages:
+            setattr(message, 'to', None)
+        assert not any([getattr(message, 'to', None) for message in test_messages])
+        with pytest.raises(Smtp2goAPIContentException):
+            email_backend.send_messages(test_messages)
 
 
-# def test_missing_from_email_field(monkeypatch, test_messages):
-#     monkeypatch.setenv('SMTP2GO_API_KEY', TEST_API_KEY)
-#     email_backend = Smtp2goEmailBackend()
-#     for message in test_messages:
-#         setattr(message, 'from', None)
-#     assert not any([message.from for message in test_messages])
-#     with pytest.raises(Smtp2goAPIContentException):
-#         email_backend.send_messages(test_messages)
+    def test_missing_from_email_field(self, monkeypatch, test_messages):
+        monkeypatch.setenv('SMTP2GO_API_KEY', TEST_API_KEY)
+        email_backend = Smtp2goEmailBackend()
+        for message in test_messages:
+            setattr(message, 'from_email', None)
+        assert not any([message.from_email for message in test_messages])
+        with pytest.raises(Smtp2goAPIContentException):
+            email_backend.send_messages(test_messages)
 
-#     monkeypatch.setenv('SMTP2GO_API_KEY', TEST_API_KEY)
-#     email_backend = Smtp2goEmailBackend()
-#     setattr(test_messages[0], 'from_email', None)
-#     with pytest.raises(Smtp2goAPIContentException):
-#         email_backend.send_messages(test_messages)
-#
-#
-# def test_missing_subject_field(monkeypatch, test_messages):
-#     monkeypatch.setenv('SMTP2GO_API_KEY', TEST_API_KEY)
-#     email_backend = Smtp2goEmailBackend()
-#     setattr(test_messages[0], 'subject', None)
-#     with pytest.raises(Smtp2goAPIContentException):
-#         email_backend.send_messages(test_messages)
-#
-#
-# def test_missing_body_field(monkeypatch, test_messages):
-#     monkeypatch.setenv('SMTP2GO_API_KEY', TEST_API_KEY)
-#     email_backend = Smtp2goEmailBackend()
-#     setattr(test_messages[0], 'body', None)
-#     with pytest.raises(Smtp2goAPIContentException):
-#         email_backend.send_messages(test_messages)
-#
-#
-# def test_fail_silently_supresses_errors(monkeypatch, test_messages):
-#     monkeypatch.setenv('SMTP2GO_API_KEY', TEST_API_KEY)
-#     email_backend = Smtp2goEmailBackend(fail_silently=True)
-#     setattr(test_messages[0], 'body', None)
-#     email_backend.send_messages(test_messages)
-#
-#
-# def test_successfully_sent_count(monkeypatch, test_messages):
-#     monkeypatch.setenv('SMTP2GO_API_KEY', TEST_API_KEY)
-#     email_backend = Smtp2goEmailBackend()
-#     monkeypatch.setattr(email_backend, '_smtp2go_send', lambda x: None)
-#     assert email_backend.send_messages(test_messages) == len(test_messages)
-#
-#
-# def test_successfully_sent_multiple_messages_count(monkeypatch, test_messages):
-#     multiplier = 10
-#     t = test_messages * multiplier
-#     monkeypatch.setenv('SMTP2GO_API_KEY', TEST_API_KEY)
-#     email_backend = Smtp2goEmailBackend()
-#     monkeypatch.setattr(email_backend, '_smtp2go_send', lambda x: None)
-#     assert email_backend.send_messages(t) == multiplier
+
+    def test_missing_subject_field(self, monkeypatch, test_messages):
+        monkeypatch.setenv('SMTP2GO_API_KEY', TEST_API_KEY)
+        email_backend = Smtp2goEmailBackend()
+        for message in test_messages:
+            setattr(message, 'subject', None)
+        assert not any([message.subject for message in test_messages])
+        with pytest.raises(Smtp2goAPIContentException):
+            email_backend.send_messages(test_messages)
+
+
+    def test_missing_body_field(self, monkeypatch, test_messages):
+        monkeypatch.setenv('SMTP2GO_API_KEY', TEST_API_KEY)
+        email_backend = Smtp2goEmailBackend()
+        for message in test_messages:
+            setattr(message, 'body', None)
+        assert not any([message.body for message in test_messages])
+        # TODO: Unify exceptions
+        with pytest.raises(Smtp2goParameterException):
+            email_backend.send_messages(test_messages)
+    #
+    # def test_successfully_sent_count(self, monkeypatch, test_messages):
+    #     monkeypatch.setenv('SMTP2GO_API_KEY', TEST_API_KEY)
+    #     email_backend = Smtp2goEmailBackend()
+    #     monkeypatch.setattr(email_backend, '_smtp2go_send', lambda x: None)
+    #     assert email_backend.send_messages(test_messages) == len(test_messages)
+    #
+    #
+    # # TODO: Mock request
+    # # def test_fail_silently_supresses_errors(monkeypatch, test_messages):
+    # #     monkeypatch.setenv('SMTP2GO_API_KEY', TEST_API_KEY)
+    # #     email_backend = Smtp2goEmailBackend(fail_silently=True)
+    # #     setattr(test_messages[0], 'body', None)
+    # #     email_backend.send_messages(test_messages)
+    # #
+    # #
+    # def test_successfully_sent_multiple_messages_count(self, monkeypatch, test_messages):
+    #     multiplier = 10
+    #     t = test_messages * multiplier
+    #     monkeypatch.setenv('SMTP2GO_API_KEY', TEST_API_KEY)
+    #     email_backend = Smtp2goEmailBackend()
+    #     monkeypatch.setattr(email_backend, '_smtp2go_send', lambda x: None)
+    #     assert email_backend.send_messages(t) == multiplier
