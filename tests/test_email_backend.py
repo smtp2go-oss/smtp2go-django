c3f4ff63d37031301a448ffaf53b83231cd51d2a
Dave Bush <github@david-bush.co.uk>
2017-04-05 10:16:59 +1200

    Added exception handling tests.

--- tests/test_email_backend.py
+++ tests/test_email_backend.py
@@ -1,4 +1,8 @@
+from smtplib import SMTPException
+from ssl import SSLError
+
 import pytest
+from smtp2go.exceptions import Smtp2goBaseException
 
 from smtp2go_django.email_backend import (
     Smtp2goEmailBackend,
@@ -37,8 +41,35 @@ class TestEmailBackend:
         messages = [test_message for i in range(count)]
         assert email_backend.send_messages(messages) == count
 
-    def test_fail_silently_supresses_errors(monkeypatch, test_message):
+    def test_fail_silently_supresses_dependency_errors(
+            self, monkeypatch, test_message):
         email_backend = Smtp2goEmailBackend(fail_silently=True)
         setattr(email_backend, '_smtp2go_send', lambda x: None)
+        # Raise exception from smtp2go dependency
         setattr(test_message, 'to', None)
+        email_backend.send_messages([test_message])
+
+    @pytest.mark.parametrize(
+        # The exceptions we're explicitly trying to catch:
+        'exception', [SMTPException, SSLError, Smtp2goBaseException])
+    def test_core_exceptions_raised(
+            self, monkeypatch, test_message, exception, email_backend):
+        def exception_raiser(exception):
+            def raiser(*args, **kwargs):
+                raise exception
+            return raiser
+        setattr(email_backend, '_smtp2go_send', exception_raiser(exception))
+        with pytest.raises(exception):
+            email_backend.send_messages([test_message])
+
+    @pytest.mark.parametrize(
+        'exception', [SMTPException, SSLError, Smtp2goBaseException])
+    def test_core_exceptions_supressed(
+            self, monkeypatch, test_message, exception):
+        email_backend = Smtp2goEmailBackend(fail_silently=True)
+        def exception_raiser(exception):
+            def raiser(*args, **kwargs):
+                raise exception
+            return raiser
+        setattr(email_backend, '_smtp2go_send', exception_raiser(exception))
         email_backend.send_messages([test_message])
